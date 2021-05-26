1aadfe78a7e5c63a90ca6fe7d1d9d19eb99bbc13
SuperQAxClub <nguyenquanganh38@gmail.com>
2021-05-26 11:22:50 +0700

    5 files modified, 5 added

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Traveller', element: 'Geo' },
+  { id: 12, name: 'Amber', element: 'Pyro' },
+  { id: 13, name: 'Kaeya', element: 'Cryo' },
+  { id: 14, name: 'Lisa', element: 'Electro' },
+  { id: 15, name: 'Zhongli', element: 'Geo' },
+  { id: 16, name: 'Tartaglia', element: 'Hydro' },
+  { id: 17, name: 'Jean', element: 'Anemo' },
+  { id: 18, name: 'Diluc', element: 'Pyro' },
+  { id: 19, name: 'Bennett', element: 'Pyro' },
+  { id: 20, name: 'Chongyun', element: 'Cryo' }
+];
